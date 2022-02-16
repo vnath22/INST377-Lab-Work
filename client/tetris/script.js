3f66c28a8295a935f1208b0953ff852dbbb2d0e3
Victoria Nathaniel <vnath447@terpmail.umd.edu>
2022-02-16 14:00:21 -0500

    added score

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10;
   let nextRandom = 0
   let timerId
+  let score = 0
 
 
   // The Tetrominoes
@@ -101,6 +102,7 @@ document.addEventListener('DOMContentLoaded', () => {
             currentPosition = 4
             draw()
             displayShape()
+            addScore()
         }
     }
 
@@ -178,12 +180,22 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    //add score
+    function addScore() {
+        for(let i = 0; i < 199; i +=width){
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
 
-    
-    // //add score
-    // function addScore() {
-    //     for()
-    // }
+            if (row.every(index => squares[index].classList.contains('taken'))){
+                score += 10
+                scoreDisplay.innerHTML = score
+                row.forEach(index => {
+                    squares[index].classList.remove('taken')
+                })
+                const squaresRemoved = squares.splice(i, width)
+                console.log(squaresRemoved)
+            }
+        }
+    }
 
 
 });
