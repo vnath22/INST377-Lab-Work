3e47143d6e341858b77194661354ce0ebae63fbc
Victoria Nathaniel <vnath447@terpmail.umd.edu>
2022-03-12 19:45:08 -0500

    submit button not properly working

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -7,7 +7,7 @@ async function mainEvent() { // the async keyword means we can make API requests
     console.log('form submission'); // this is substituting for a "breakpoint"
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
     const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
+    console.table(arrayFromJson); // this is called "dot notation"
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it contains all 1,000 records we need
   });
--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,32 +1,60 @@
-// As the last step of your lab, hook this up to index.html
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-template-curly-in-string */
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(
+    Math.random() * (newMax - newMin + 1) + newMin
+  ); // The maximum is inclusive and the minimum is inclusive
+}
 
+function restoArrayMake(dataArray) {
+  console.log('fired dataHandler');
+  // console.table(dataArray); // this is called "dot notation"
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[restNum];
+  });
+
+  return listItems;
+}
+
+function createHtmlList(collection) {
+  console.log('fired HTML creator');
+  console.log('collection');
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = '<li>${displayName}</li>';
+    targetList.innerHTML += injectThisItem;
+  });
+}
+
+// As the last step of your lab, hook this up to index.html
 async function mainEvent() { // the async keyword means we can make API requests
+  console.log('script loaded'); // substituting for a 'breakpoint'
   const form = document.querySelector('.main_form');
   const submit = document.querySelector('.submit_button');
+
   const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-  });
-  function dataHandler(dataArray) {
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  submit.style.display = 'none'; // it is better not to display this until the data has loaded
 
-  }
+  console.log(arrayFromJson);
 
-let root = document.documentElement;
-
-root.addEventListener("button", e => {
-root.style.setProperty('--mouse-x', e.clientX + "px");
-root.style.setProperty('--mouse-y', e.clientY + "px");
-
-display: none
-});
-
-  :button{
-    display: none;
+  if (arrayFromJson.data > 0) {
+    submit.style.display = 'block';
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+      const restoArray = restoArrayMake(arrayFromJson.data);
+      createHtmlList(restoArray);
+    });
   }
 }
 
